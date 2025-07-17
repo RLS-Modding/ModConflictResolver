fc0ffa4b5b724da11079d5b913f116553e0430b6
TristanGaldys <123184923+TristanGaldys@users.noreply.github.com>
2025-07-17 16:27:51 -0400

    - Removing RepoModManager

--- scripts/ModConflictResolver/modScript.lua
+++ scripts/ModConflictResolver/modScript.lua
@@ -1,8 +1,3 @@
--- Load the dependency management system
-setExtensionUnloadMode("requiredMods", "manual")
-extensions.unload("requiredMods")
-
--- Load the conflict resolver system
 setExtensionUnloadMode("conflictResolver", "manual")
 extensions.unload("conflictResolver")
 
